fca5885266ced8b796e48957c0871847a6d96522
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2022-07-06 11:58:45 -0300

    Update runtime to fdo 21.08

--- Carcara-launcher.sh
+++ Carcara-launcher.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-MACHINE_TYPE=`uname -m`
-if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-  exec "/app/carcara_lin_6_1.5.x86_64"
-else
-  exec "/app/carcara_lin_6_1.5.x86"
-fi
-
+exec "/app/game/carcara_lin_6_1.5.x86_64"
